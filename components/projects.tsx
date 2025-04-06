c121d492f98686b061661ebafd8f3c3335adc112
Nathaniel Viana <nathanielviana@gmail.com>
2025-04-06 10:33:49 -0400

    Fixed the mobile look for the projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -36,41 +36,40 @@ export default function Projects() {
 }
 type ProjectProps = (typeof projectsData)[0];
 
-function Project({title,description, tags, imageUrl}: ProjectProps)
-{
+function Project({title, description, tags, imageUrl}: ProjectProps) {
     return(
-    <section className="group bg-gray-100 max-w-[42rem] rounded-lg border
-    border-black/5 overflow-hidden sm:pr-8 relative sm:h-[25rem]
-     mb-3 sm:mb-8 last:mb-0 hover:bg-gray-200 transition">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10
-        sm:max-w-[50%] flex flex-col h-full">
-            <h3 className="text-2xl font-semibold">{title}</h3>
-            <p className="mt-2 leading-relaxed text-gray-700 mb-2">{description}</p>
-            <ul className="flex flex-wrap mt-2 gap-2 sm:mt-auto mb-2">
-                {tags.map((tag, index) => (
-                    <li
-                        className="bg-black/[0.7] px-3
-                        py-1 text-[0.7rem] uppercase
-                        tracking-wider text-white
-                        rounded-full font-medium"
-                        key={index}>{tag}</li>
-                ))}
-            </ul>
-        </div>
-        <section>
-        <Image className="absolute bottom-0 -right-40
-               w-[28.25rem] rounded-t-lg shadow-2xl h-[18rem]
-               transition
-               group-hover:-translate-x-3
-               group-hover:translate-y-3
-               group-hover:-rotate-2"
+        <section className="group bg-gray-100 max-w-[42rem] rounded-lg border
+    border-black/5 overflow-hidden relative sm:pr-8 sm:h-[25rem]
+    mb-3 sm:mb-8 last:mb-0 hover:bg-gray-200 transition">
+            <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10
+      sm:max-w-[50%] flex flex-col h-full">
+                <h3 className="text-2xl font-semibold">{title}</h3>
+                <p className="mt-2 leading-relaxed text-gray-700 mb-2">{description}</p>
+                <ul className="flex flex-wrap mt-2 gap-2 sm:mt-auto mb-2">
+                    {tags.map((tag, index) => (
+                        <li
+                            className="bg-black/[0.7] px-3
+              py-1 text-[0.7rem] uppercase
+              tracking-wider text-white
+              rounded-full font-medium"
+                            key={index}>{tag}</li>
+                    ))}
+                </ul>
+            </div>
 
-
-               src={imageUrl}
-
-               alt="Projects I've worked on"
-               quality={95} />
+            {/* Mobile & Desktop image with responsive classes */}
+            <Image
+                className="w-full h-64 object-cover sm:object-contain mt-4 sm:mt-0
+        sm:absolute sm:bottom-0 sm:-right-40 sm:w-[28.25rem] sm:h-[18rem]
+        sm:rounded-t-lg sm:shadow-2xl
+        transition
+        sm:group-hover:-translate-x-3
+        sm:group-hover:translate-y-3
+        sm:group-hover:-rotate-2"
+                src={imageUrl}
+                alt="Projects I've worked on"
+                quality={95}
+            />
         </section>
-    </section>
     );
 }
